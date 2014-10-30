38f6e3577b190610cd7d7969dea91e833b74d317
Leo Simkin <leo.m.simkin@gmail.com>
2014-10-30 00:08:31 -0700

    Initial

---
+++ Assets/Scripts/BookManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookManagerScript : MonoBehaviour {
+
+	bool bookStateGrid = false; // False = Map, True = grid 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		GUI.BeginGroup (new Rect (0, Screen.height / 2, Screen.width, Screen.height / 2));
+
+		GUI.Box (new Rect (0, 0, Screen.width, Screen.height / 2), "The Book is Here");
+
+		GUI.EndGroup ();
+	}
+
+}
